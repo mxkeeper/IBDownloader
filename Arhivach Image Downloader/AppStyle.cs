81b7165b2c726c61194fb07f42d70ebeee874f08
mxkeeper <mxkeeper@gmail.com>
2015-11-20 22:22:03 +0300

    no message

---
+++ Arhivach Image Downloader/AppStyle.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Arhivach_Image_Downloader
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // get the theme from the current application
+            var theme = ThemeManager.DetectAppStyle(Application.Current);
+
+            // now set the Green accent and dark theme
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Blue"),
+                                        ThemeManager.GetAppTheme("BaseDark"));
+
+            base.OnStartup(e);
+        }
+    }
+}
